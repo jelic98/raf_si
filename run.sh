5796141e8e2b610ecefb2d7d1e356f4f0f718254
Lazar Jelic <lazarjelic98@gmail.com>
2021-04-11 22:51:16 +0200

    Stopiranje svih procesa preko porta

--- run.sh
+++ run.sh
@@ -42,37 +42,37 @@ rm -rf "$PATH_LOG" && mkdir "$PATH_LOG"
 
 log "Starting $BROKER_DIR on port $BROKER_PORT"
 nohup php -S 127.0.0.1:$BROKER_PORT -t "$BROKER_DIR" > "$PATH_LOG/$BROKER_DIR.txt" 2>&1 &
-BROKER_PID=$(lsof -t -i:$BROKER_PORT)
 
 log "Starting $LOG_DIR on port $LOG_PORT"
 nohup php -S 127.0.0.1:$LOG_PORT -t "$LOG_DIR" > "$PATH_LOG/$LOG_DIR.txt" 2>&1 &
-LOG_PID=$(lsof -t -i:$LOG_PORT)
 
 log "Starting $AUTH_DIR on port $AUTH_PORT"
 nohup mvn -f "$AUTH_DIR/pom.xml" spring-boot:run > "$PATH_LOG/$AUTH_DIR.txt" 2>&1 &
-AUTH_PID=$(lsof -t -i:$AUTH_PORT)
 
 log "Starting $CORE_DIR on port $CORE_PORT"
 nohup mvn -f "$CORE_DIR/pom.xml" spring-boot:run > "$PATH_LOG/$CORE_DIR.txt" 2>&1 &
-CORE_PID=$(lsof -t -i:$CORE_PORT)
 
 log "Starting $CLIENT_DIR on port $CLIENT_PORT"
 nohup npm run serve --prefix "$CLIENT_DIR" -- --port $CLIENT_PORT > "$PATH_LOG/$CLIENT_DIR.txt" 2>&1 &
-CLIENT_PID=$(lsof -t -i:$CLIENT_PORT)
 
 waitforq
 
+CLIENT_PID=$(lsof -t -i:$CLIENT_PORT)
 log "Stopping $CLIENT_DIR (PID $CLIENT_PID) on port $CLIENT_PORT"
 kill -9 $CLIENT_PID
 
+CORE_PID=$(lsof -t -i:$CORE_PORT)
 log "Stopping $CORE_DIR (PID $CORE_PID) on port $CORE_PORT"
 kill -9 $CORE_PID
 
+AUTH_PID=$(lsof -t -i:$AUTH_PORT)
 log "Stopping $AUTH_DIR (PID $AUTH_PID) on port $AUTH_PORT"
 kill -9 $AUTH_PID
 
+LOG_PID=$(lsof -t -i:$LOG_PORT)
 log "Stopping $LOG_DIR (PID $LOG_PID) on port $LOG_PORT"
 kill -9 $LOG_PID
 
+BROKER_PID=$(lsof -t -i:$BROKER_PORT)
 log "Stopping $BROKER_DIR (PID $BROKER_PID) on port $BROKER_PORT"
 kill -9 $BROKER_PID
