97da8dddf19029f937aa8e86a6198a96bbf56d78
Lazar Jelic <lazarjelic98@gmail.com>
2021-05-06 22:09:21 +0200

    [FIX] Manuelno parsiranje PUT i DELETE parametara na brokeru

--- Broker/routes/api.php
+++ Broker/routes/api.php
@@ -21,7 +21,7 @@
 		$data = $_REQUEST;
 	}else {
 		$data = [];
-		parse_str(file_get_contents("php://input"), $data);
+		parse_raw_http_request($data);
 	}
 
 	$curl = curl_init();
@@ -45,4 +45,27 @@
 
 	http_response_code($code);
 	echo $response;
+
+	function parse_raw_http_request(array &$a_data) {
+		$input = file_get_contents('php://input');
+		preg_match('/boundary=(.*)$/', $_SERVER['CONTENT_TYPE'], $matches);
+		$boundary = $matches[1];
+
+		$a_blocks = preg_split("/-+$boundary/", $input);
+		array_pop($a_blocks);
+
+		foreach($a_blocks as $id => $block) {
+			if(empty($block)) {
+				continue;
+			}
+
+			if(strpos($block, 'application/octet-stream') !== FALSE) {
+				preg_match("/name=\"([^\"]*)\".*stream[\n|\r]+([^\n\r].*)?$/s", $block, $matches);
+			}else {
+				preg_match('/name=\"([^\"]*)\"[\n|\r]+([^\n\r].*)?\r$/s', $block, $matches);
+			}
+
+			$a_data[$matches[1]] = $matches[2];
+		}
+	}
 ?>
