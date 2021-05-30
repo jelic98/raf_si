16dff57dd4c2a4d737456d61b179f3f4ebd48670
Lazar Jelic <lazarjelic98@gmail.com>
2021-05-30 20:25:35 +0200

    Inicijalna implementacija transformera

--- Transformer/src/rs/raf/transformer/Transformer.java
+++ Transformer/src/rs/raf/transformer/Transformer.java
@@ -1,16 +1,65 @@
 package rs.raf.transformer;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class Transformer {
 
     public JSONObject transform(String model) {
+        JSONObject fum = new JSONObject();
+
+        JSONObject rqm = new JSONObject(model);
+        JSONObject details = rqm.getJSONObject("details");
+        JSONArray requirements = details.getJSONArray("requirements");
+        JSONArray actors = details.getJSONArray("actors");
+
+        int key = 0;
+
+        JSONArray nodes = new JSONArray();
+
+        for (int i = 0; i < actors.length(); i++) {
+            JSONObject actor = actors.getJSONObject(i);
+
+            JSONObject node = new JSONObject();
+            node.put("img", "https://cdn.discordapp.com/attachments/558738094795522049/839263865623019551/unknown.png");
+            node.put("category", "actor");
+            node.put("key", key++);
+            node.put("text", actor.getString("name"));
+
+            nodes.put(node);
+        }
+
+        addRequirements(requirements, nodes, key);
+
         JSONObject id = new JSONObject();
+        id.put("name", String.format("%s - fum %d", rqm.getJSONObject("_id").getString("name"), System.currentTimeMillis() / 1000L));
+        id.put("project", rqm.getJSONObject("_id").getString("project"));
 
-        // TODO Transform RQM to FUM
-        // TODO Save FUM to MongoDB
-        // TODO Return FUM id
+        fum.put("_id", id);
+        fum.put("nodes", nodes);
 
-        return id;
+        return fum;
+    }
+
+    private void addRequirements(JSONArray requirements, JSONArray nodes, int key) {
+        for (int i = 0; i < requirements.length(); i++) {
+            JSONObject requirement = requirements.getJSONObject(i);
+
+            boolean functional = requirement.getString("requirement_type").equals("functional");
+
+            if (!functional) {
+                continue;
+            }
+
+            JSONObject node = new JSONObject();
+            node.put("img", "https://cdn.discordapp.com/attachments/558738094795522049/839263865623019551/unknown.png");
+            node.put("category", "use_case");
+            node.put("key", key++);
+            node.put("text", requirement.getString("title"));
+
+            addRequirements(requirement.getJSONArray("children"), nodes, key);
+
+            nodes.put(node);
+        }
     }
 }
